4cc4266b3e2d50c72529ddac6c353aa21df3da48
Selen <selenyanar@gmail.com>
2021-10-20 17:03:30 +0300

    Initial Commit

---
+++ TwitterSplashScreen/TwitterSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSplashScreenApp.swift
+//  TwitterSplashScreen
+//
+//  Created by Selen Yanar on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
